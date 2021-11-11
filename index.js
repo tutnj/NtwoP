5fa2350d418b97002d66cfe528af547709d9f563
SravanthT <83528948+SravanthT@users.noreply.github.com>
2021-11-11 21:45:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function handleclick() {
+  window.location.href =
+    "https://docs.google.com/forms/d/e/1FAIpQLScfvzMOx3P_aby106Vadoh92FV8xvMd-Jr6rMpEoNwGi8EFzg/viewform?usp=sf_link";
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+  document.getElementById("place").classList.toggle("change");
+  document.getElementById("menubar").classList.toggle("change");
+}
